8929825d4ae8ac10f7b6e358c1b29dce6a511bf6
Kelvin Furfles <kelvinsilva.ksv@gmail.com>
2019-07-13 17:36:11 -0300

    ajuste life time

--- src/utils/tokenGenerator.ts
+++ src/utils/tokenGenerator.ts
@@ -2,7 +2,7 @@ export{}
 const jwt = require('jsonwebtoken')
 
 const tokenGenerator = (payload) => {
-    return jwt.sign(payload, process.env.SECRET, { expiresIn: 10000 })
+    return jwt.sign(payload, process.env.SECRET, { expiresIn: process.env.TOKEN_LIFE_TIME })
 }
 
 const tokenValidator = (token) => {
